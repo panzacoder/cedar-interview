5796a418240a8bb736768cfeccb4c39b30c0a33f
Jake Hebert <jacobshebert@gmail.com>
2024-07-08 15:51:27 -0500

    chore: formatting pass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Cedar FE Interview | Jake Hebert",
-  description: "An applicationfor the Cedar FE Design Systems Interview, by Jake Hebert. Features NextJS, TypeScript, and TailwindCSS.",
-};
+  title: 'Cedar FE Interview | Jake Hebert',
+  description:
+    'An applicationfor the Cedar FE Design Systems Interview, by Jake Hebert. Features NextJS, TypeScript, and TailwindCSS.'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
